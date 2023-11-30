0d925562812ace2655aecd18fd167274cf709662
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2023-11-30 11:11:25 +0530

    Update RabbitMqUtil.java

--- src/main/java/com/visualpathit/account/utils/RabbitMqUtil.java
+++ src/main/java/com/visualpathit/account/utils/RabbitMqUtil.java
@@ -1,4 +1,4 @@
-package com.visualpathit.account.utils;
+package com.vcube.account.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
