26b6e366959dafecb728abdfe676b119c9d36e32
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2023-11-30 11:06:17 +0530

    Update UserServiceImpl.java

--- src/main/java/com/visualpathit/account/service/UserServiceImpl.java
+++ src/main/java/com/visualpathit/account/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
-package com.visualpathit.account.service;
+package com.vcube.account.service;
 
-import com.visualpathit.account.model.User;
-import com.visualpathit.account.repository.RoleRepository;
-import com.visualpathit.account.repository.UserRepository;
+import com.vcube.account.model.User;
+import com.vcube.account.repository.RoleRepository;
+import com.vcube.account.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
