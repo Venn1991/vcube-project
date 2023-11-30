861523b77aa7bf29d272d39018135b24de26562c
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2023-11-30 11:07:17 +0530

    Update UserService.java

--- src/main/java/com/visualpathit/account/service/UserService.java
+++ src/main/java/com/visualpathit/account/service/UserService.java
@@ -1,8 +1,8 @@
-package com.visualpathit.account.service;
+package com.vcube.account.service;
 
 import java.util.List;
 
-import com.visualpathit.account.model.User;
+import com.vcube.account.model.User;
 
 /** {@author waheedk}!*/
 public interface UserService {
